8df6296075197c140cd99f5fbd91d78353a91f6e
JosepAnSabaté <josep.andreu@e-campus.uab.cat>
2021-10-18 16:31:23 +0200

    atributs

--- mapa.js
+++ mapa.js
@@ -1,5 +1,13 @@
 function init() {
     const map = L.map('map', {
+      // drawControl: true, // draw tools, more down
+      // draw: {
+      //   circle: false,
+      //   marker: true,
+      //   polyline: false, 
+      //   polygon: false,
+      //   rectangle: false
+      // },
       center: [41.6863, 1.6382],
       zoom: 8,
       attribution: 'Institut Cartogràfic i Geològic de Catalunya CC-BY-SA-3'
@@ -31,19 +39,52 @@ function init() {
       //FULL SCREEN 
       L.control.fullscreen().addTo(map);
       // map coordinates mouse
-    
     map.on('mousemove', function(e) {
       //console.log(e)
       const latitude = e.latlng.lat.toFixed(5);
       const longitude = e.latlng.lng.toFixed(5);
       $('.coordinate').html(`Latitud: ${latitude}     Longitud:${longitude}`)
     })
+
+    // DRAWN MARKERS//
+  // initiate a variable to store the drawn items:
+  // FeatureGroup is to store editable layers
+  let drawnItems = new L.FeatureGroup();
+  map.addLayer(drawnItems);
+
+//create drawing controls and toolbar
+  const drawControl = new L.Control.Draw({
+    draw: {
+      circle: false,
+      marker: true,
+      polyline: false, 
+      polygon: false,
+      rectangle: false
+    }
+  }).addTo(map);
+
+
+  //  capture the data that is drawn using  event on Leaflet called ‘draw:created’
+  map.on('draw:created', (e) => {
+    // Each time we create a feature(point, line or polygon), we add this feature to the feature group wich is drawnItems in this case
+    drawnItems.addLayer(e.layer);
+
+    drwanItemsGJ = drawnItems.toGeoJSON();
+    //console.log(drwanItemsGJ.features)
+    console.log(drwanItemsGJ.features[0].geometry); // get geometry
+    console.log(drwanItemsGJ.features[0].geometry.coordinates[0]);  //get coordinate x
+    const coords = e.layer._latlng;
+    console.log(coords) // get coordinates
+  });
+  
 }
+
 
 // Full screen map view
 const mapId = document.getElementById('map');
 function fullScreenView(){
   mapId.requestFullscreen();
 }
+
 
 
